6cea4bf5abc5e13612fcc470169cc822122b4bfa
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-07-26 19:45:19 +0200

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //Uses HtmlEncoder.Default.Encode to protect the app from malicious input, such as through JavaScript.
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
+
